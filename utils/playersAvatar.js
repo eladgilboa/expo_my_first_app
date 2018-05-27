323963d78205248fbef91d534f20c77bc53c34fd
elad.gilboa <elad.gilboa@ladbrokescoral.com>
2018-05-27 14:27:31 +0300

    add player avatars...

--- utils/playersAvatar.js
+++ utils/playersAvatar.js
@@ -7,6 +7,16 @@ const playersAvatar = [
   require('../images/realPlayersAvatar/messi.jpeg'),
   require('../images/realPlayersAvatar/baggio.png'),
   require('../images/realPlayersAvatar/suarez.png'),
+  require('../images/realPlayersAvatar/falcao.jpg'),
+  require('../images/realPlayersAvatar/gullit.png'),
+  require('../images/realPlayersAvatar/iniesta.jpg'),
+  require('../images/realPlayersAvatar/maradona.jpg'),
+  require('../images/realPlayersAvatar/neymar.jpg'),
+  require('../images/realPlayersAvatar/ronaldinho.jpg'),
+  require('../images/realPlayersAvatar/valderrama.png'),
+  require('../images/realPlayersAvatar/van_persi.png'),
+  require('../images/realPlayersAvatar/zidan.jpeg'),
+  require('../images/realPlayersAvatar/zlatan.png'),
 ];
 
 export default playersAvatar
